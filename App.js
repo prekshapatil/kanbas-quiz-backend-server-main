7aeb105a41206d3f290e4858a6e57688d9d1a732
prekshapatil <patil.pre@northeastern.edu>
2024-04-21 23:14:49 -0400

    cors

--- App.js
+++ App.js
@@ -11,6 +11,11 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://merry-bavarois-7d6051.netlify.app/#/Labs',
+  ];
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL
@@ -27,10 +32,12 @@ app.use(express.json());
         cookie: {
           sameSite: "none",
           secure: true,
-          domain: "kanbas-server-app-project.onrender.com",
+          domain: "kanbas-quiz-backend-server-main.onrender.com",
         },
       })
     ); 
+
+
 
 ModuleRoutes(app);
 CourseRoutes(app);
@@ -38,9 +45,26 @@ QuestionRoutes(app);
 QuizRoutes(app);
 UserRoutes(app);
 
+const db = mongoose.connection;
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+console.log(process.env.DB_CONNECTION_STRING);
+
+
+
 app.get("/health_check", (req, res) => {
     res.json({ message: "Life is good!!!" });
     }
 );
 
-app.listen(4000);
+app.listen(4000 || process.env.PORT);
