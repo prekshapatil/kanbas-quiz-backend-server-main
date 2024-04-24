dfa1e15f5caa33547f1600c58260b73fbb1f4074
prekshapatil <patil.pre@northeastern.edu>
2024-04-24 13:54:29 -0400

    working it local

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import "dotenv/config";
 import QuizRoutes from "./Kanbas/quizzes/routes.js";
 import QuestionRoutes from "./Kanbas/questions/routes.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 
@@ -23,20 +23,17 @@ app.use(
   })
 );
 
-
-
 const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
+  secret: "some secret",
   saveUninitialized: false,
+  resave: false,
 };
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
-  }
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+  })
+ );
   
 app.use(session(sessionOptions));
 
